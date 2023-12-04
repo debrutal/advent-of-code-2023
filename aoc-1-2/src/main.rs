d48ab24a5e5469d2ff03682017cf3f91ac578e1c
Dennis Winter <dennis.winter.ext@hermesworld.com>
2023-12-04 11:28:05 +0100

    still wrong...

--- aoc-1-2/src/main.rs
+++ aoc-1-2/src/main.rs
@@ -1,5 +1,13 @@
-use std::cmp::{max, min};
+use std::cmp::max;
 use std::fs::read_to_string;
+
+const TEST_SAMPLE: &str = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
 
 const NUMBER_STRINGS: [&'static str; 9] = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
@@ -7,9 +15,10 @@ const NUMBER_STRINGS: [&'static str; 9] = [
 
 fn main() {
     let path = "input/input.txt";
-    let input = read_to_string(&path).expect(&format!("Could not read file {}", &path).to_string());
+    let _input =
+        read_to_string(&path).expect(&format!("Could not read file {}", &path).to_string());
 
-    get_numbers_and_sum(input);
+    //    get_numbers_and_sum(input);
     get_numbers_and_sum(TEST_SAMPLE.to_string());
 }
 fn get_numbers_and_sum(input: String) {
@@ -29,60 +38,104 @@ fn get_numbers(input: String) -> Vec<u32> {
 // also interprets written characters as number e.g. one, five, etc. up to nine
 fn get_first_and_last_number(line: String) -> u32 {
     print!("{line} ");
-    let (first, index_of_first) = find_first(&line);
-    let (last, index_of_last) = find_last(&line);
+    let result: String = match find_first(&line) {
+        None => "0".to_string(),
+        Some(first) => {
+            let last = find_last(&line).expect("This should not happen since find_first already found something!");
+            let number_as_str: String = if first.1 == last.1 {
+                println!("adding {}", first.1);
+                format!("{}", first.1).to_string()
+            } else {
+                println!("adding {}{}", first.1, last.1);
+                format!("{}{}", first.1, last.1).to_string()
+            };
 
-    let number = if index_of_first == index_of_last {
-        println!("adding {first}");
-        format!("{}", first)
-    } else {
-        println!("adding {first}{last}");
-        format!("{}{}", first, last)
+            number_as_str
+        }
     };
 
-    number
+    result
         .parse::<u32>()
-        .expect(&format!("not a number {number}").to_string())
+        .expect(&format!("not a number {result}"))
 }
 
-fn find_first(line: &String) -> (String, usize) {
-    let number_index = line.chars().position(|e| e.is_digit(10)).unwrap() as usize;
-    print!("fi = {number_index} ");
-    let mut result = line[number_index..number_index + 1].to_string();
-    let mut lowest_index = usize::MAX;
+fn find_first(line: &String) -> Option<(String, usize)> {
+    let optional_number_index = line.chars().position(|e| e.is_digit(10));
+    let mut result: Option<(String, usize)> = match optional_number_index {
+        Some(number_index) => {
+            print!("fi = {number_index} ");
+            Some((line[number_index..=number_index].to_string(), number_index))
+        }
+        None => None,
+    };
     for word in NUMBER_STRINGS.iter() {
         if let Some(word_found_on_index) = line.find(word) {
-            if word_found_on_index < lowest_index && word_found_on_index < number_index {
-                lowest_index = word_found_on_index;
-                let number = convert_word_to_number(word.to_string());
-                result = format!("{:}", number);
+            result = match result {
+                Some(inner_result) => {
+                    if word_found_on_index < inner_result.1 {
+                        let number = convert_word_to_number(word.to_string());
+                        Some((format!("{:}", number), word_found_on_index))
+                    } else {
+                        Some(inner_result)
+                    }
+                }
+                None => {
+                    let number = convert_word_to_number(word.to_string());
+                    Some((format!("{:}", number), word_found_on_index))
+                }
             }
         }
     }
-    (result, min(lowest_index, number_index))
+    result
 }
 
-fn find_last(line: &String) -> (String, usize) {
-    let number_index =
-        line.len() - line.chars().rev().position(|e| e.is_digit(10)).unwrap() as usize - 1;
-    print!("li: {number_index} ");
+fn find_last(line: &String) -> Option<(String, usize)> {
+    let optional_number_index = line.chars().rev().position(|e| e.is_digit(10));
 
-    let mut result = line[number_index..=number_index].to_string();
+    let mut result: Option<(String, usize)> = match optional_number_index {
+        None => None,
+        Some(index) => {
+            let number_index =line.len() - index as usize - 1;
+            print!("li: {number_index} ");
+
+            Some((line[number_index..=number_index].to_string(), number_index))
+        }
+    };
 
     let mut lowest_index = usize::MIN;
+
     for word in NUMBER_STRINGS.iter() {
-        let indices: Vec<usize> = line.match_indices(word).map(|(i, _)| i).collect();
-        if indices.len() > 0 {
-            let word_found_on_index = indices[indices.len() - 1];
-            if word_found_on_index > lowest_index && word_found_on_index > number_index {
-                lowest_index = word_found_on_index;
-                let number = convert_word_to_number(word.to_string());
-                result = format!("{:}", number);
+        if let Some(word_found_on_index) = line.find(word) {
+            result = match result {
+                Some(inner_result) => {
+                    if word_found_on_index > inner_result.1 {
+                        let number = convert_word_to_number(word.to_string());
+                        Some((format!("{:}", number), word_found_on_index))
+                    } else {
+                        Some(inner_result)
+                    }
+                }
+                None => {
+                    let number = convert_word_to_number(word.to_string());
+                    Some((format!("{:}", number), word_found_on_index))
+                }
             }
         }
     }
-    (result, max(lowest_index, number_index))
+    //     for word in NUMBER_STRINGS.iter() {
+    //         let indices: Vec<usize> = line.match_indices(word).map(|(i, _)| i).collect();
+    //         if indices.len() > 0 {
+    //             let word_found_on_index = indices[indices.len() - 1];
+    //             if word_found_on_index > lowest_index && word_found_on_index > number_index {
+    //                 lowest_index = word_found_on_index;
+    //                 let number = convert_word_to_number(word.to_string());
+    //                 result = format!("{:}", number);
+    //             }
+    //         }
+    //     }
+    result
 }
+
 fn convert_word_to_number(word: String) -> usize {
     print!("t ");
     NUMBER_STRINGS
@@ -91,10 +144,3 @@ fn convert_word_to_number(word: String) -> usize {
         .unwrap()
         + 1
 }
-const TEST_SAMPLE: &str = "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen";

67a42a77f0ed3e76b656fdc7de5ce503905b85dd
Dennis Winter <duennes@gmail.com>
2023-12-06 13:21:30 +0100

    update 1-2 - remove input (sorry for that)

--- aoc-1-2/src/main.rs
+++ aoc-1-2/src/main.rs
@@ -1,4 +1,3 @@
-use std::cmp::max;
 use std::fs::read_to_string;
 
 const TEST_SAMPLE: &str = "two1nine
@@ -15,11 +14,10 @@ const NUMBER_STRINGS: [&'static str; 9] = [
 
 fn main() {
     let path = "input/input.txt";
-    let _input =
-        read_to_string(&path).expect(&format!("Could not read file {}", &path).to_string());
+    let input = read_to_string(&path).expect(&format!("Could not read file {}", &path).to_string());
 
-    //    get_numbers_and_sum(input);
-    get_numbers_and_sum(TEST_SAMPLE.to_string());
+    get_numbers_and_sum(input);
+    //    get_numbers_and_sum(TEST_SAMPLE.to_string());
 }
 fn get_numbers_and_sum(input: String) {
     let numbers: Vec<u32> = get_numbers(input);
@@ -41,13 +39,14 @@ fn get_first_and_last_number(line: String) -> u32 {
     let result: String = match find_first(&line) {
         None => "0".to_string(),
         Some(first) => {
-            let last = find_last(&line).expect("This should not happen since find_first already found something!");
-            let number_as_str: String = if first.1 == last.1 {
-                println!("adding {}", first.1);
-                format!("{}", first.1).to_string()
+            let last = find_last(&line)
+                .expect("This should not happen since find_first already found something!");
+            let number_as_str: String = if first.0 == last.0 {
+                println!("adding {}", first.0);
+                format!("{}", first.0).to_string()
             } else {
-                println!("adding {}{}", first.1, last.1);
-                format!("{}{}", first.1, last.1).to_string()
+                println!("adding {}{}", first.0, last.0);
+                format!("{}{}", first.0, last.0).to_string()
             };
 
             number_as_str
@@ -63,19 +62,25 @@ fn find_first(line: &String) -> Option<(String, usize)> {
     let optional_number_index = line.chars().position(|e| e.is_digit(10));
     let mut result: Option<(String, usize)> = match optional_number_index {
         Some(number_index) => {
-            print!("fi = {number_index} ");
-            Some((line[number_index..=number_index].to_string(), number_index))
+            let actual_number = line[number_index..=number_index].to_string();
+
+            print!("fi_n = {number_index}; value = {actual_number} ");
+            Some((actual_number, number_index))
         }
         None => None,
     };
     for word in NUMBER_STRINGS.iter() {
         if let Some(word_found_on_index) = line.find(word) {
+            print!("fi_w = {word_found_on_index} ");
             result = match result {
                 Some(inner_result) => {
                     if word_found_on_index < inner_result.1 {
                         let number = convert_word_to_number(word.to_string());
+                        print!("; w_value = {number} ");
+                        print!("## {word}");
                         Some((format!("{:}", number), word_found_on_index))
                     } else {
+                        print!("AAAA: {}", inner_result.1);
                         Some(inner_result)
                     }
                 }
@@ -95,14 +100,12 @@ fn find_last(line: &String) -> Option<(String, usize)> {
     let mut result: Option<(String, usize)> = match optional_number_index {
         None => None,
         Some(index) => {
-            let number_index =line.len() - index as usize - 1;
+            let number_index = line.len() - index as usize - 1;
             print!("li: {number_index} ");
 
             Some((line[number_index..=number_index].to_string(), number_index))
         }
     };
-
-    let mut lowest_index = usize::MIN;
 
     for word in NUMBER_STRINGS.iter() {
         if let Some(word_found_on_index) = line.find(word) {
